1eabbf1bf25cfb32df371ed0042d7b2a1bd13b5f
Yu <yuhwachn@gmail.com>
2022-06-22 21:01:43 -0400

    add middleware

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
-
+const PORT = 8000
 let db,
     dbConnectionString = process.env.DB_STRING,
     dbName = 'sample_mflix',
@@ -15,3 +15,13 @@ MongoClient.connect(dbConnectionString)
         db = client.db(dbName)
         collection = db.collection('movies')
     })
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server is running on port`)
+})
